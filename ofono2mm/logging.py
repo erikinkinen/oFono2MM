5c8cec9dac5c8cdd0398ede0409dcde6bc173691
Bardia Moshiri <fakeshell@bardia.tech>
2024-04-30 20:36:44 -0400

    logging: don't try to print caller name if self is not in caller frame
    incase one wants to build the code using something like cython, this will let it run because cython works a bit differently on caller frames
    
    Signed-off-by: Bardia Moshiri <fakeshell@bardia.tech>

--- ofono2mm/logging.py
+++ ofono2mm/logging.py
@@ -6,8 +6,12 @@ def ofono2mm_print(message, verbose):
 
     frame = inspect.currentframe()
     caller_frame = frame.f_back
-    cls_name = caller_frame.f_locals['self'].__class__.__name__
-    func_name = caller_frame.f_code.co_name
+    if 'self' in caller_frame.f_locals:
+        cls_name = caller_frame.f_locals['self'].__class__.__name__
+        func_name = caller_frame.f_code.co_name
+        full_message = f"{cls_name}.{func_name}: {message}"
+    else:
+        func_name = caller_frame.f_code.co_name
+        full_message = f"{func_name}: {message}"
 
-    full_message = f"{cls_name}.{func_name}: {message}"
     print(full_message)
